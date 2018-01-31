3c87d577adf320a5a765e7bf24f349923b7e305f
Simon Brady <simonbrady@mac.com>
2018-01-31 17:14:43 -0500

    Splash added

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import TextLoop from 'react-text-loop';
 
-
 class Splash extends React.Component {
   render() {
     return (
       <div>
         Splash
         <div className='page'>
-          <TextLoop springConfig={{ stiffness: 180, damping: 8 }}
-                    children={[
-                      "Full Stack",
-                      "Front End",
-                      "Back End",
-                      "Web"
-                    ]} 
-                    speed={1800}>
-          </TextLoop> Developer
+          <div className='title'>
+            <TextLoop springConfig={{ stiffness: 180, damping: 8 }}
+                      children={[
+                        "Full Stack",
+                        "Front End",
+                        "Back End",
+                        "Web"
+                      ]} 
+                      speed={2300}>
+            </TextLoop> Developer
+          </div>
         </div>
       </div>
     )
